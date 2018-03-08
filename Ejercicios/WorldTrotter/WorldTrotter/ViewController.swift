ac8c160ee10f4e94c1a76293954adb62c40fef58
Heslak <ser_acosta_95@hotmail.com>
2018-03-07 19:46:43 -0800

    Agregado Proyecto WorldTrotter

---
+++ Ejercicios/WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Sergio Acosta Vega on 3/7/18.
+//  Copyright © 2018 seedvi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    /*override func viewDidLoad() {
+        super.viewDidLoad()
+        let firstFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
+        let firstView = UIView(frame: firstFrame)
+        firstView.backgroundColor = UIColor.blue
+        view.addSubview(firstView)
+        
+        let secondFrame = CGRect(x: 20, y: 30, width: 50, height: 50)
+        let secondView = UIView(frame: secondFrame)
+        secondView.backgroundColor = UIColor.green
+        //view.addSubview(secondView)
+        firstView.addSubview(secondView)
+        
+    }*/
+    
+}
+
