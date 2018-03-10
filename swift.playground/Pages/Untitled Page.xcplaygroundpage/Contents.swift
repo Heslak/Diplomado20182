b5f2e4abd02905979c85ff149f58062e12f4fefb
Sergio Acosta <ser_acosta_95@hotmail.com>
2018-03-10 09:43:30 -0600

    Agregado .gitignore

---
+++ swift.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+//: [Previous](@previous)
+
+import UIKit
+
+
+enum Día {
+    case Lunes
+    case Martes
+    case Miércoles
+    case Jueves
+    case Viernes
+}
+
+var díaSemana: Día = Día.Lunes
+print(díaSemana)
+
+
+//: [Next](@next)
