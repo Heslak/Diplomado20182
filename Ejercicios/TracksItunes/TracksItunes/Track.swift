6ee1c85eaab85b8cdf79848069d64eaac3c98e08
Sergio Acosta <ser_acosta_95@hotmail.com>
2018-04-27 21:01:47 -0500

    Clase 27/04/18

---
+++ Ejercicios/TracksItunes/TracksItunes/Track.swift
@@ -0,0 +1,14 @@
+//
+//  Track.swift
+//  TracksItunes
+//
+//  Created by d182_Sergio_A on 27/04/18.
+//  Copyright © 2018 Seedvi. All rights reserved.
+//
+
+import Foundation
+
+struct Track:Decodable {
+    let trackName: String
+    let artistName: String
+}
