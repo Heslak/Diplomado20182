0fa02a250e7787ebc0ef2c4cf08ceab502d7a808
Sergio Acosta <ser_acosta_95@hotmail.com>
2018-05-26 13:42:03 -0500

    Clase 26/05/18

--- Ejercicios/ReorderCell/ReorderCell/TableViewController.swift
+++ Ejercicios/ReorderCell/ReorderCell/TableViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class TableViewController: UITableViewController {
 
+    var alumnos = ["Jorge","Sofi","Edgar","Virgi","Sergio","Donovan"]
+    var borrados: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,36 +20,136 @@ class TableViewController: UITableViewController {
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "reuseIdentifier")
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return alumnos.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        
+        cell.textLabel?.text = alumnos[indexPath.row]
         // Configure the cell...
 
         return cell
     }
-    */
+ 
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction =  UIContextualAction(style: .destructive, title: "Delete") { (action, sourceView, completionHandler) in
+            self.borrados.append(self.alumnos[indexPath.row])
+            self.alumnos.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .fade)
+            completionHandler(true)
+        }
+        let shareAction = UIContextualAction(style: .normal, title: "Compartir") { (action, sourceView, completionHandler) in
+            let message = "Compartiendo al alummno:"+self.alumnos[indexPath.row]
+            let activityController = UIActivityViewController(activityItems: [message], applicationActivities: nil)
+            self.present(activityController, animated: true, completion: nil)
+            if let popoverController = activityController.popoverPresentationController {
+                if let cell = tableView.cellForRow(at: indexPath){
+                    popoverController.sourceView = cell
+                    popoverController.sourceRect = cell.bounds
+                }
+            }
+        }
+        
+        deleteAction.backgroundColor = UIColor(red: 231/255, green: 76/255, blue: 60/255, alpha: 1.0)
+        deleteAction.image = UIImage(named: "Delete")
+        
+         shareAction.backgroundColor = UIColor(red: 254/255, green: 149/255, blue: 38/255, alpha: 1.0)
+        shareAction.image = UIImage(named: "Share")
+        let swipeConfiguration = UISwipeActionsConfiguration(actions: [deleteAction,shareAction])
+        
+        return swipeConfiguration
+    }
+    
+    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        if borrados.count == 0 { return nil}
+        
+        let addAction =  UIContextualAction(style: .normal, title: "Agregar ultimo renglón") { (action, sourceView, completionHandler) in
+            self.alumnos.append(self.borrados.popLast()!)
+            self.tableView.reloadData()
+            completionHandler(true)
+        }
+        
+        
+        let swipeConfiguration = UISwipeActionsConfiguration(actions: [addAction])
 
+        return swipeConfiguration
+    }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        
+        return .none
+    }
+    
+    override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let movedAlumno = self.alumnos.remove(at: sourceIndexPath.row)
+        self.alumnos.insert(movedAlumno, at: destinationIndexPath.row)
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        let rowData = self.alumnos[indexPath.row]
+        return rowData.hasPrefix("S")
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let alertController = UIAlertController(title: "Marcando alumnos", message: "¿Desea marcarlo?", preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        
+        let okAction = UIAlertAction(title: "Ok", style: .default) { (action) in
+            let cell = self.tableView.cellForRow(at: indexPath)
+            if cell?.accessoryType == .checkmark{
+                cell?.accessoryType = .none
+            }else{
+                cell?.accessoryType = .checkmark
+            }
+        }
+        
+        alertController.addAction(cancelAction)
+        alertController.addAction(okAction)
+        present(alertController,animated: true,completion: nil)
+    }
+    
+   /* override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.alpha = 0
+        UIView.animate(withDuration: 1.0) {
+            cell.alpha = 1
+        }
+    }*/
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        //let rotationAngleinRadians = 90 * CGFloat(Double.pi/180)
+        
+        //let rotationTransform = CATransform3DMakeRotation(rotationAngleinRadians,0,0,1)
+        
+        let rotationTransform = CATransform3DTranslate(CATransform3DIdentity, -500, 100, 0)
+        
+        cell.layer.transform = rotationTransform
+        
+        UIView.animate(withDuration: 1.0) {
+            cell.layer.transform = CATransform3DIdentity
+        }
+        
+    }
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -60,12 +163,12 @@ class TableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            alumnos.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
-    }
-    */
+    }*/
 
     /*
     // Override to support rearranging the table view.
